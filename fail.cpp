5285c0c3d187f9fe97cefbe11a942ffd252086a1
Júlia Jamnická <xjamnick@fi.muni.cz>
2020-12-03 14:43:17 +0100

    first bug fixed

--- fail.cpp
+++ fail.cpp
@@ -10,7 +10,8 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
   for (int i = 0; i <= 5; i++) Stack [i] = 0;
 
   int* array = new int[5];
-  array[100] = 0; 
+  
+  array[2] = 0; 
 
   arrayStack[100] = 0; 
   arrayHeap[100] = 0; 
